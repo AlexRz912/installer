dd2f2ceb4f98344dac38291be9704400b15d07da
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-03-15 21:40:27 +0100

    implemented_simple_tool_install

--- simple_install.sh
+++ simple_install.sh
@@ -4,6 +4,6 @@ source ./installation/install_tools.sh
 # source ./utils/script_utils.sh
 
 enable_standard_logging $1
-tools_install nano vim code git $?
+tools_install nano vim code git tldr $?
 
 
