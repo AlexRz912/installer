cf50e40b3460728e819018388f144217dfe04813
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-03-16 23:57:22 +0100

    fixing_last_minute_errors

--- utils/tool_install_util.sh
+++ utils/tool_install_util.sh
@@ -6,8 +6,7 @@ source ./logs/err.sh
 curl_install_list=(code)
 
 tool_install() {
-    
-    [ $2 -eq 0 || $1 = "git" ] && verbose $1 || silent $1
+    [[ $2 -eq 0 || $1 == "git" ]] && verbose "$1" || silent "$1"
 }
 
 silent() {
@@ -34,4 +33,6 @@ install_vscode() {
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
+
+    prompt_success "visual studio code successfully installed"
 }
