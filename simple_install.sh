85236336e66183cdfb5036209938f7b17e067d58
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-03-16 12:27:13 +0100

    cleaned_main_script

--- simple_install.sh
+++ simple_install.sh
@@ -3,13 +3,12 @@ source ./utils/enable_standard_log_util.sh
 source ./installation/install_tools.sh
 
 enable_standard_logging $1
-
 enabled=$?
+
 tools_install nano vim code git tldr $enabled
-
-# [ $2 = "simple" ] && ssh-keygen -t rsa -b 4096
-
 zshell_install $enabled
 
+[ $2 = "simple" ] && ssh-keygen -t rsa -b 4096
+# man pages
 
 
